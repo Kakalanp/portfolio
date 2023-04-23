59d774cd663a3dd083e94101ffd4d2746f8e7cac
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2023-04-22 20:51:10 -0500

    Add Nav skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Nav from'./sections/nav';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>Visuals</li>
+        <li>Algorithms</li>
+        <li>combos</li>
+      </ul>
+      <div className="Theme"></div>
+    </div>
+  )
+}
+
+export default Nav;
